537a2497b1ed24f44b0016c9937c9aca53e2f71a
Ignacio <ignaciosilvadeleon@gmail.com>
2026-04-25 17:08:16 +1000

    add input_practice

---
+++ exercises/input_practice.py
@@ -0,0 +1,8 @@
+# 1-Necesito un imput "What is your name " y asignarlo a una variable name 
+# 2- Necesito un imput "How old are you " y asignarlo a una variable age 
+# 3- Necesito un print con f string para crear texto flexible con variables
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+print(f"Hello {name}, you are {age} years old")
+print(type(age))
