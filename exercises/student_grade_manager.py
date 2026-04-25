f99da4398c40d010e49ecc068def9a7a1c3a1e9e
Ignacio <ignaciosilvadeleon@gmail.com>
2026-04-25 12:09:54 +1000

    add new student and save passing score as variable

--- exercises/student_grade_manager.py
+++ exercises/student_grade_manager.py
@@ -18,16 +18,19 @@
 students = [
     {"name": "Ignacio", "age": 34, "grades": [8, 9, 10]},
     {"name": "Maria", "age": 31, "grades": [10, 11, 12]},
-    {"name": "Billy", "age": 20, "grades": [7, 11, 10]}
+    {"name": "Billy", "age": 20, "grades": [7, 11, 10]},
+    {"name": "Mandy", "age": 26, "grades": [4, 5, 8]}
 ]
 
 # Creá una función llamada get_average que reciba una lista de notas y retorne el promedio
 def get_average(grades):
     return sum(grades) / len(grades)
-    
+
+passing_score = 7
+
 # Recorré la lista de estudiantes con un loop y para cada estudiante:
 for student in students:
     average = get_average(student["grades"])
-    if average >= 6:
+    if average >= passing_score:
         print(f"{student["name"]} passed with an average of {average}")
     else: print(f"{student["name"]} failed with an average of {average}")
