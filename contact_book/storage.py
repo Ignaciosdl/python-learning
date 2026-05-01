9c053f9772518f1f07e5ddbab2d37b93ddd46154
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-01 14:50:00 +1000

    add storage module with load and save functions with comments

--- contact_book/storage.py
+++ contact_book/storage.py
@@ -3,16 +3,16 @@ from pathlib import Path # Import path from library pathlib
 
 # load saved contacts at startup
 def load_contacts():
-    file_path = Path("contacts.json") 
-    if file_path.is_file(): 
-        with open(file_path, "r") as file:
-            contacts = json.load(file)
+    file_path = Path("contacts.json") # Create the path to the file contact.json
+    if file_path.is_file(): # Does this path has a file?
+        with open(file_path, "r") as file: # If exists then open it in read mode
+            contacts = json.load(file) # load the information and store it in the variable contacts
     else:
-        contacts = []
+        contacts = [] # If the file doesnt exist then create the variable contact as an empty list
     return contacts
 
-# save contacts at completion
-def save_contacts(contacts):
-    file_path = Path("contacts.json")
-    with open(file_path, "w") as file:
-        json.dump(contacts, file)
+# serialize contacts list and save to json file
+def save_contacts(contacts): 
+    file_path = Path("contacts.json") # Look for the file path
+    with open(file_path, "w") as file: # Open it in write mode
+        json.dump(contacts, file) # Save all the changes on a json file
