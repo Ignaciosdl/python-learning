c0e62a21bf035af3f583afd61dc3420c4175ebf6
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-02 16:55:43 +1000

    add delete_contact function

--- contact_book/contacts.py
+++ contact_book/contacts.py
@@ -1,4 +1,3 @@
-
 from storage import load_contacts, save_contacts # import the the functions from storage.py module
 
 contacts = load_contacts() # calling the function load_contacts and add it to contacts variable
@@ -13,8 +12,29 @@ def search_contact(contacts, name):
     for contact in contacts: # iterating the list contacts
         if contact["name"] == name: # if the name is in the list shows:
             print(f"Name: {contact["name"]}, Phone: {contact["phone"]}, Email: {contact["email"]}") 
-            found = True # change the value of the variable to describe that the name was founde
+            found = True # change the value of the variable to describe that the name was found
             break # once the contact was founded by the name we stop the loop
     if found ==False:
         print(f"{name} is not in the list of contact") # if the contact was not found once the loop finished whe show this
-    
+
+# function to delete a dictionaries from a list
+def delete_contact(contacts,name):
+    found = False # create variable to describe that the name was not found yet
+    for contact in contacts: # iterating the list contacts
+        if contact["name"] == name: # if the name is in the list
+            while True: # the program will be run until the breaks stop it
+                    delete = input(f"Are you sure you want to delete this contact Name: {contact["name"]} y/n?")
+                    if delete == "y": # if the user want to delete it
+                        print(f"The contact: Name: {contact["name"]}, Phone: {contact["phone"]}, Email: {contact["email"]} have been removed")
+                        contacts.remove(contact) # delete the dictionary from the contacts list
+                        found = True # change the value of the variable to describe that the name was found
+                        break # once the contact is removed the program stop
+                    elif delete == "n": # if the user dont want to delet it
+                        print(f"Deletion cancelled")
+                        found = True
+                        break
+                    else:
+                        print("Please enter y or n")
+    if found == False: # if the contact is not founded after the complete iteration
+        print(f"{name} is not in the list of contact") # if the contact was not found once the loop finished whe show this
+        
--- contact_book/main.py
+++ contact_book/main.py
@@ -1,7 +1,9 @@
-from contacts import contacts, add_contact, search_contact # import the the functions from contacts.py module
+from contacts import contacts, add_contact, search_contact, delete_contact # import the the functions from contacts.py module
 
 add_contact("Ignacio","303456","igna@tuemial.com")
 search_contact(contacts,"Ignacio")
 search_contact(contacts,"Maria")
 
 print(contacts)
+delete_contact(contacts,"Ignacio")
+print(contacts) 
