25e610baa81009398f36c917122b2ef6a8546988
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-04 15:22:37 +1000

    add avaliable_budget function

--- budget_traker/budget.py
+++ budget_traker/budget.py
@@ -6,3 +6,9 @@ def set_budget(budget,amount): # function receives two parameters budget and amo
 # function to add dictionaries with name, category and amout to the expenses list
 def add_expense(expenses, name, category, amount): 
     expenses.append({"name":name, "category":category, "amount":amount}) # adding dcitionaries to list with append method
+
+# function to calculate the availabel budget    
+def available_budget(budget, expenses): # receives two parameters budget and expenses
+    total_expenses =  sum(map(lambda expense: expense["amount"], expenses)) # extract amount from each expense and calculate total with map() and sum()
+    available_budget = budget - total_expenses # rest the total of expenses from the mensual budget
+    return available_budget # return the available budget
--- budget_traker/main.py
+++ budget_traker/main.py
@@ -1,8 +1,10 @@
-from budget import set_budget, add_expense
+from budget import set_budget, add_expense, available_budget
 
 budget = 0
 budget = set_budget(budget, 1000)
 print(budget)
 expenses = []
 add_expense(expenses,"banana","food",5)
+add_expense(expenses,"fuel","transport",15)
 print(expenses)
+print(available_budget(budget,expenses))
