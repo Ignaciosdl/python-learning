5035aa87fe9e186fa49ee903c4b46607444e15d0
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-04 14:51:31 +1000

    add add_expense function

--- budget_traker/budget.py
+++ budget_traker/budget.py
@@ -2,4 +2,7 @@
 def set_budget(budget,amount): # function receives two parameters budget and amount
     budget = amount # asign the amount as a parameter to the variable budget
     return budget # return budget with the new amount value
-    
+
+# function to add dictionaries with name, category and amout to the expenses list
+def add_expense(expenses, name, category, amount): 
+    expenses.append({"name":name, "category":category, "amount":amount}) # adding dcitionaries to list with append method
--- budget_traker/main.py
+++ budget_traker/main.py
@@ -1,5 +1,8 @@
-from budget import set_budget
+from budget import set_budget, add_expense
 
 budget = 0
 budget = set_budget(budget, 1000)
 print(budget)
+expenses = []
+add_expense(expenses,"banana","food",5)
+print(expenses)
