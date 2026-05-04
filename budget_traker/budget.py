23dbe54e8cf4482e343dec12a66d9d8db3583ec4
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-04 14:26:51 +1000

    add set_budget function

---
+++ budget_traker/budget.py
@@ -0,0 +1,5 @@
+# function to modify the budget amount
+def set_budget(budget,amount): # function receives two parameters budget and amount
+    budget = amount # asign the amount as a parameter to the variable budget
+    return budget # return budget with the new amount value
+    
---
+++ budget_traker/main.py
@@ -0,0 +1,5 @@
+from budget import set_budget
+
+budget = 0
+budget = set_budget(budget, 1000)
+print(budget)
