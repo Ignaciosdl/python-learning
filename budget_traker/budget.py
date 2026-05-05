6be92853fd2d7d03cb16f016d0a668521c1feb81
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-05 10:36:23 +1000

    refactor available_budget to use get_total_expenses

--- budget_traker/budget.py
+++ budget_traker/budget.py
@@ -7,9 +7,14 @@ def set_budget(budget,amount): # function receives two parameters budget and amo
 def add_expense(expenses, name, category, amount): 
     expenses.append({"name":name, "category":category, "amount":amount}) # adding dcitionaries to list with append method
 
-# function to calculate the availabel budget    
-def available_budget(budget, expenses): # receives two parameters budget and expenses
+# function to calculate the total amount of expenses    
+def get_total_expenses(expenses): # receives the expenses list a parameter
     total_expenses =  sum(map(lambda expense: expense["amount"], expenses)) # extract amount from each expense and calculate total with map() and sum()
+    return total_expenses # return the total amount of expenses
+
+# function to calculate the availabel budget    
+def available_budget(budget,expenses): # receives two parameters budget and expenses
+    total_expenses =  get_total_expenses(expenses) # call the function get_total_expenses to calculate the total amount of expenses
     available_budget = budget - total_expenses # rest the total of expenses from the mensual budget
     return available_budget # return the available budget
 
@@ -22,3 +27,4 @@ def category_expenses(expenses):
         else: # if the category that we are looking for is part of the categories_total dictionarie then:
             categories_total[expense["category"]] = categories_total[expense["category"]] + expense["category"] # we add the amount to the value of the existing key
     return categories_total
+
