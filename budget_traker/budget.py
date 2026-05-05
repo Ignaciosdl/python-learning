6eb5c47e5177ae2c46a5a089019488701e277325
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-05 12:05:12 +1000

    refactor set_budget function

--- budget_traker/budget.py
+++ budget_traker/budget.py
@@ -1,7 +1,8 @@
 # function to modify the budget amount
-def set_budget(budget,amount): # function receives two parameters budget and amount
-    budget = amount # asign the amount as a parameter to the variable budget
-    return budget # return budget with the new amount value
+def set_budget(amount): # function receives two parameters budget and amount
+    if amount <= 0: # if the amount is equal or smaller than zero 
+        raise ValueError("Budget must be positive")  # # raise error if amount is invalid
+    return amount # if amount is positive is valid
 
 # function to add dictionaries with name, category and amout to the expenses list
 def add_expense(expenses,name,category,amount): 
