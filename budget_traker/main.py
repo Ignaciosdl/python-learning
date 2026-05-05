1d9f12ec823488b76629e2133ca989e7420fffdd
Ignacio <ignaciosilvadeleon@gmail.com>
2026-05-05 12:41:55 +1000

    add menu structure and budget_input function

--- budget_traker/main.py
+++ budget_traker/main.py
@@ -1,29 +1,43 @@
 from budget import set_budget, add_expense, available_budget, category_expenses, budget_alert
 from storage import load_data, save_data
 
+def budget_input():
+    while True:
+        try:
+            amount = int(input(f"Please set a menusal budget amount: "))
+            budget = set_budget(amount)
+            break
+        except ValueError: 
+            print("Please enter a positive number")
+    return budget 
 
-budget = set_budget(-100)
-print(budget)
-expenses = []
-add_expense(expenses,"banana","food", 45)
-add_expense(expenses,"fuel", "transport", 35)
-print(expenses)
-print(available_budget(budget, expenses))
-print(category_expenses(expenses))
-budget_alert(budget,expenses)
+def menu_input():
+        while True:
+            try:
+                option = int(input(f"""
+                === BUDGET TRAKER ===
+                Please enter one of the following numbers:
+                1. Set budget
+                2. Add expenses
+                3. View total expenses
+                4. View availabe budget 
+                5. View expenses by category
+                6. Exit
+                """))
+                if option == 1:
+                   budget = budget_input()
 
-# def menu_input():
-#     while True:
-#         try:
-#             option = int(input(f"""
-#             === BUDGET TRAKER ===
-#             Please enter one of the following numbers:
-#             1. Set budget
-#             2. Add expenses
-#             3. View total expenses
-#             4. View availabe budget 
-#             5. View expenses by category
-#             6. Exit
-#             """))
-#             if option == 1:
-#                 budget =
+budget, expenses = load_data()
+# budget = set_budget(100)
+# print(budget)
+# expenses = []
+# add_expense(expenses,"banana","food", 45)
+# add_expense(expenses,"fuel", "transport", 35)
+# print(expenses)
+# print(available_budget(budget, expenses))
+# print(category_expenses(expenses))
+# budget_alert(budget,expenses)
+
+if budget == 0:
+    budget = budget_input()
+                           
